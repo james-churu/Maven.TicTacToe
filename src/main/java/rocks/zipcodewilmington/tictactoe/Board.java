681857b7f29bf640e0759298a0b34c11e1a2c8f3
jchuru <jameschuru123@gmail.com>
2020-02-13 17:07:01 -0500

    All tests Passed

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,81 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+    Character[][] board;
+
+    public Board(Character[][] user) {
+        board = user;
     }
+
 
     public Boolean isInFavorOfX() {
-        return null;
+        if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+            return true;
+        } else if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
+            return true;
+        } else if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') {
+            return true;
+        } else if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') {
+            return true;
+        } else if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') {
+            return true;
+        } else if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') {
+            return true;
+        } else if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+            return true;
+        } else if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
+
     public Boolean isInFavorOfO() {
-        return null;
+
+        if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') {
+            return true;
+        } else if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') {
+            return true;
+        } else if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') {
+            return true;
+        } else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') {
+            return true;
+        } else if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') {
+            return true;
+        } else if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') {
+            return true;
+        } else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+            return true;
+        } else if (board[0][2] == 'O' && board[1][1] == 'O' && board[1][0] == 'O') {
+            return true;
+        } else {
+            return false;
+        }
+
+
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfX() || isInFavorOfO()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public String getWinner() {
-        return null;
+
+        String answer = "";
+
+        if (isInFavorOfX()) {
+            answer = "X";
+        } else if (isInFavorOfO()) {
+            answer = "O";
+        }
+        return answer;
     }
+
 
 }
